141e4aafc32602820adffeac36f2e73038eafae2
Puri Sahib <61573288+purisahib@users.noreply.github.com>
2021-03-17 19:38:49 +0530

    Add files via upload

---
+++ CameraMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    // variables 
+    float rotationOnX;
+    float mouseSensitivity = 90f;
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Taking mouse input
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSensitivity;
+        float m_X = Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensitivity;
+
+        // Rotate camera up and down
+        rotationOnX -= mouseY;
+        rotationOnX = Mathf.Clamp(rotationOnX, -90f, 90f);
+        transform.localEulerAngles = new Vector3(rotationOnX, 0f, 0f);
+
+        // Rotate Left and right
+        player.Rotate(Vector3.up * m_X);
+    }
+}
